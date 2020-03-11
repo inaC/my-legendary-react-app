24cbe32489d3ef80749e08eddeaa0816af28d1c1
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-03-11 14:28:24 -0300

    Updates

---
+++ precache-manifest.15dea8be70ded1ead0336e74536e621c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b643b2db38c9c409ed19a44d7113a3",
+    "url": "/my-legendary-react-app/index.html"
+  },
+  {
+    "revision": "17a7861713e224aeda48",
+    "url": "/my-legendary-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef01acf6705483a5ade6",
+    "url": "/my-legendary-react-app/static/js/2.0b34e45c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-legendary-react-app/static/js/2.0b34e45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a7861713e224aeda48",
+    "url": "/my-legendary-react-app/static/js/main.e9282d8a.chunk.js"
+  },
+  {
+    "revision": "f17e8854fd69d346bed7",
+    "url": "/my-legendary-react-app/static/js/runtime-main.269e1e2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-legendary-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
